759c24828c7d2236ccec807673f4165087acd79f
Your Name <you@example.com>
2022-08-24 17:45:54 +0700

    reformat date in json

--- src/main/java/com/icom/cart/entity/ItemEntity.java
+++ src/main/java/com/icom/cart/entity/ItemEntity.java
@@ -1,5 +1,6 @@
 package com.icom.cart.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.GenericGenerator;
@@ -34,9 +35,11 @@ public class ItemEntity {
     private String transactionStatus;
 
     @Column(name = "created_at")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss.SS")
     private LocalDateTime createdAt;
 
     @Column(name = "updated_at")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss.SS")
     private LocalDateTime updatedAt;
 
 }
